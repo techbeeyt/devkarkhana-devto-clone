284261962d732ff330f055efc257b8d46b5d2a0d
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-07-21 15:34:37 +0600

    Right Bar done

--- src/components/RightSideBar/ListingItems.jsx
+++ src/components/RightSideBar/ListingItems.jsx
@@ -2,13 +2,13 @@ import React from 'react';
 
 const ListingItems = ({ item }) => {
   const { title, category } = item;
-  return <div className='group flex flex-col p-4 hover:bg-white '>
-    <h1 className='text-gray-800 group-hover:text-blue-800 mb-1'>{title}</h1>
+  return <button className='group flex flex-col p-4 hover:bg-white'>
+    <h1 className='text-gray-800 group-hover:text-blue-800 mb-1 text-start'>{title}</h1>
     {
       category === "New" ? <span className="w-fit px-[3px] py-[2px] rounded-md text-[#6b2c03] text-sm bg-yellow-300">{category}</span>
-        : <p className="text-gray-600 text-sm ">{category}</p>
+        : <p className="w-fit text-gray-600 text-sm">{category}</p>
     }
-  </div>
+  </button>
 };
 
 export default ListingItems;
