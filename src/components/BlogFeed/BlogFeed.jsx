4da517847bae9cd45b1a6925399b56fdb5ed3bba
Montasir Mahmud <mmcse19@gmail.com>
2022-07-21 15:42:44 +0600

    Final Touch

--- src/components/BlogFeed/BlogFeed.jsx
+++ src/components/BlogFeed/BlogFeed.jsx
@@ -1,40 +1,36 @@
 import React from 'react';
 import { posts } from '../../assets/database/posts';
-import { GiSelfLove } from 'react-icons/gi'
-import { FaRegComment } from 'react-icons/fa'
+import { RiHeart2Line } from 'react-icons/ri'
+import Comments from '../../assets/svg/blogfeed/comments.svg';
+import { Link } from 'react-router-dom';
+import { toSlug } from '../../Control/toSlug';
+
 const BlogFeed = () => {
 
-    console.log(posts)
-
-
     return (
-        <div className=" min-h-full">
-            {/* top section of BlogFeed */}
+        <div className="min-h-full flex flex-col justify-center w-full">
             <section>
-                <button className="mx-0.5 text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100  hover:bg-white hover:text-indigo-600 focus:font-bold ">
+                <button className="mx-0.5 text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100  hover:bg-white hover:text-indigo-600">
                     Relevant
                 </button>
 
-                <button className=" mx-0.5 text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100  hover:bg-white hover:text-indigo-600 focus:font-bold ">
+                <button className=" mx-0.5 text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100  hover:bg-white hover:text-indigo-600">
                     Latest
                 </button>
-                <button className="mx-0.5  text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100 hover:bg-white hover:text-indigo-600 focus:font-bold ">
+                <button className="mx-0.5  text-gray-700 py-1.5 px-2.5 rounded text-lg bg-gray-100 hover:bg-white hover:text-indigo-600">
                     Top
                 </button>
-
             </section>
             {/* BlogFeed Main section */}
-            <section className='mt-5 '>
-
+            <section className='mt-5'>
                 {
                     posts.map(post =>
-
-                        <div key={post.postID} className="card mb-2 mr-5 bg-white border-solid border-2 border-gray-200 active:border-indigo-600  bg-base-100 ">
-
-                            <figure><img src={post.img} alt="" /></figure>
+                     <Link to={`/${post.userID}/${toSlug(post.post_title)}`}>
+                        <div key={post.postID} className="rounded-md overflow-hidden mb-2 mr-5 bg-white border border-gray-300 active:outline active:outline-3 active:outline-indigo-600">
+                            {
+                                post.img ? (<figure className="-mt-1"><img src={post.img} alt=""/></figure>) : null
+                            }
                             <div className="card-body">
-
-
                                 <div className='flex flex-row items-start' >
                                     <div className="avatar w-8">
                                         <div className="w-24 rounded-full">
@@ -48,45 +44,29 @@ const BlogFeed = () => {
                                 </div>
 
                                 <div className='ml-2 sm:ml-10'>
-
                                     <h1 className="card-title text-3xl font-bold hover:text-indigo-600">{post.post_title}</h1>
-
-
                                     <p>
 
                                         {post.tags.map(tag =>
-
-
                                             <button key={tag.length} className='text-sm py-1 px-1.5 rounded border-solid border-2  border-white hover:bg-gray-100 hover:border-gray-200 '>#{tag}</button>)}
-
-
-
                                     </p>
-
 
                                     <p className='text-left'> {post.post_body}</p>
 
-
-
                                     <div className="card-actions justify-between ml-0 mt-3">
                                         <div className='flex flex-row '>
-                                            <button className='p-1.5 items-center rounded hover:bg-gray-100 flex flex-row'><GiSelfLove></GiSelfLove> <span className='px-2'>{post.hearts} <span className='hidden sm:inline'>Reactions</span></span></button>
-                                            <button className='p-1.5 items-center  rounded hover:bg-gray-100 flex flex-row'> <FaRegComment></FaRegComment> <span className='px-2'>{post.comments.count} <span className='hidden sm:inline'>Comments</span></span></button>
+                                            <button className='p-1.5 items-center rounded hover:bg-gray-100 flex flex-rowv text-gray-600'><RiHeart2Line size={20}/> <span className='px-2'>{post.hearts} <span className='hidden sm:inline'>Reactions</span></span></button>
+                                            <button className='p-1.5 items-center  rounded hover:bg-gray-100 flex flex-row text-gray-600'> <img src={Comments} /> <span className='px-2'>{post.comments.count} <span className='hidden sm:inline'>Comments</span></span></button>
                                         </div>
                                         <button className="mx-0.5  text-gray-700 py-1.5 px-2.5 rounded-lg  bg-gray-200 hover:bg-gray-300 ">Save</button>
                                     </div>
                                 </div>
-
-
                             </div>
-
-
-                        </div>)
+                        </div>
+                    </Link>    
+                    )
                 }
-
-
             </section >
-
         </div >
     );
 };
