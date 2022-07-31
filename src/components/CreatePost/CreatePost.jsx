e82e4e676f6f66ae9ecb8feabc2be51b41f4065b
techbeeyt <mmcse19@gmail.com>
2022-07-31 20:59:07 +0600

    Create Post Page

--- src/components/CreatePost/CreatePost.jsx
+++ src/components/CreatePost/CreatePost.jsx
@@ -1,13 +1,29 @@
 import React from 'react'
 import { Link } from 'react-router-dom';
+import { ReactSVG } from 'react-svg';
+
 import logo from '../../assets/logo.png';
 import { MdClose } from 'react-icons/md';
+import { BsThreeDotsVertical } from 'react-icons/bs';
+import bold from '../../assets/svg/createpost/bold.svg';
+import code from '../../assets/svg/createpost/code.svg';
+import codeblock from '../../assets/svg/createpost/codeblock.svg';
+import embed from '../../assets/svg/createpost/embed.svg';
+import heading from '../../assets/svg/createpost/heading.svg';
+import italic from '../../assets/svg/createpost/italic.svg';
+import link from '../../assets/svg/createpost/link.svg';
+import list123 from '../../assets/svg/createpost/list123.svg';
+import listdot from '../../assets/svg/createpost/listdot.svg';
+import quote from '../../assets/svg/createpost/quote.svg';
+import uploadimage from '../../assets/svg/createpost/uploadimage.svg';
+import HoverDiv from './../HoverDiv/HoverDiv';
+import ToolTip from './../ToolTip/ToolTip';
 
 const CreatePost = () => {
   return (
     <div className='w-screen h-screen absolute top-0 left-0 right-0 bottom-0 z-30 bg-zinc-50'>
-        <div className='w-full flex justify-start items-center mx-32'>
-            <div className='lg:w-5/6 flex justify-center items-center py-2 px-6'>
+        <div className='w-full flex justify-start items-center md:mx-4 lg:mx-32'>
+            <div className='md:w-full lg:w-5/6 flex justify-center items-center py-2 px-6'>
                 <Link to="/" className="mr-4">
                     <img src={logo} alt="logo" width="50" />
                 </Link>
@@ -20,14 +36,98 @@ const CreatePost = () => {
             <button><MdClose /></button>
         </div>
 
-        <div className='mx-56 flex justify-between gap-10'>
+        <div className='mx-4 md:mx-20 lg:mx-56 flex justify-between gap-10'>
           <div className='bg-white border border-gray-300 w-3/4 rounded-lg px-14 py-8 flex flex-col'>
-            <button className='px-3 py-1.5 border-2 border-gray-300 rounded-md hover:shadow-md w-min whitespace-nowrap mb-4'>Add a Cover Image</button>
-            <input type='text' className='text-5xl text-gray-800 font-extrabold focus:outline-none' placeholder='New post title is here...'/>
-            <input type='text' className='focus:outline-none my-2' placeholder="Add upto 4 tags" />
+            <ToolTip title="Upload image with 100:42 ratio for better result">
+              <button className='px-3 py-1.5 border-2 border-gray-300 rounded-md hover:shadow-md w-min whitespace-nowrap mb-4'>Add a Cover Image</button>
+            </ToolTip>
+            <input type='text' className='text-5xl text-gray-800 font-extrabold focus:outline-none' placeholder='New post title is here...' />
+            <input type='text' className='focus:outline-none mt-4 mb-2 font-Cairo tracking-wider' placeholder="Add upto 4 tags" />
 
-            <div className='bg-gray-100 -mx-14 px-14 py-4 my-4'>
-              <button>B</button>
+            <div className='flex justify-between items-center bg-gray-100 -mx-14 px-14 pt-3 pb-1 my-4'>
+              <div className='flex justify-start items-center'>
+                <ToolTip title="Bold (ctrl+b)">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={bold} className="hover:text-indigo-600" />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Italic (ctrl+i)">
+                  <HoverDiv>
+                    <button className='hover:text-indigo-600'>
+                      <ReactSVG src={italic} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Add Link">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={link} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Ordered List">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={list123} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Unordered List">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={listdot} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                
+                <ToolTip title="Heading">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={heading} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Quote">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={quote} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Code">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={code} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Codeblock">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={codeblock} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Embed">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={embed} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+                <ToolTip title="Upload Image">
+                  <HoverDiv>
+                    <button>
+                      <ReactSVG src={uploadimage} />
+                    </button>
+                  </HoverDiv>
+                </ToolTip>
+              </div>
+              <button className='mb-2'>
+                <BsThreeDotsVertical size={22}/>
+              </button>
             </div>
             <textarea placeholder='Write your post content here...' className='font-Cairo focus:outline-none h-56 text-lg tracking-wide'/>
           </div>
--- src/components/ToolTip/ToolTip.jsx
+++ src/components/ToolTip/ToolTip.jsx
@@ -2,24 +2,21 @@ import React, { useState } from 'react';
 
 const ToolTip = ({ title, children }) => {
     const [show, setShow] = useState(false);
+    const [showing, setShowing] = useState('opacity-0  -translate-y-4');
     
 
     const mouseEnter = () => {
-        setShow(true);
+        setShowing('opacity-1 translate-y-0 delay-300');
     }
     const mouseLeave = () => {
-        setShow(false);
+        setShowing('opacity-0 -translate-y-2');
     }
     return (
         <div className="relative" onMouseEnter={mouseEnter} onMouseLeave={mouseLeave}>  
             {children}
-            {
-                show ? (
-                    <div className="absolute px-2 rounded-sm bg-gray-800 -bottom-5 left-4">
-                        <span className="text-xs text-gray-200">{title}</span>
-                    </div>
-                ) : null
-            }
+            <div className={`${showing} transition-all duration-300 absolute px-2 py-1 rounded-md bg-gray-800 -bottom-10 left-0`}>
+                        <span className="text-gray-200 whitespace-nowrap">{title}</span>
+            </div>
         </div>
     );
 };
