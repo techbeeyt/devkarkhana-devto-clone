5a7116030aa7ed9b6bf2798fb6916766f9df4a37
Swapnil-Safkat <shahriarswapnil50@gmail.com>
2022-08-05 14:30:49 +0600

    restart podcast

--- src/components/Podcast/Podcast.jsx
+++ src/components/Podcast/Podcast.jsx
@@ -3,37 +3,39 @@ import { Link } from 'react-router-dom';
 
 const Podcast = () => {
   return (
-    <div className='w-5/6 min-h-screen mx-auto p-8'>
-      <div className='w-full flex justify-between'>
-        <h1 className='text-black text-3xl font-bold mb-8'>Notifications</h1>
-        <Link to='/' className='w-fit my-6 py-2 px-4 border-0 text-gray-700 font-normal cursor-pointer rounded-lg hover:bg-gray-200 hover:text-black'>Suggest a podcast</Link>
-      </div>
-      <section className='w-full'>
-        <div className='w-full'>
-          <h1 className='text-black text-xl font-bold'>Latest episodes</h1>
-          <div>
-          </div>
-          {
-            
-          }
+    <div className='w-full min-h-screen'>
+      <div className='w-full lg:w-2/3 mx-auto h-80'>
+        <div className='w-full flex justify-between items-center p-2 pt-4'>
+          <h1 className='text-black text-2xl sm:text-3xl font-bold'>Podcasts</h1>
+          <Link to='/settings' className='w-fit mr-4 border-0 text-gray-700 font-normal cursor-pointer rounded-lg hover:bg-gray-200 hover:text-black'>Settings</Link>
         </div>
-        <div className='w-full'>
-          <h1 className='text-black text-xl font-bold'>Featured shows</h1>
-          <div>
+        <section className='w-full flex flex-col sm:flex-row'>
+          <div className='w-full'>
+            <h1 className='text-black text-xl font-bold'>Latest episodes</h1>
+            <div>
+            </div>
             {
 
             }
           </div>
-        </div>
-        <div className='w-full'>
-          <h1 className='text-black text-xl font-bold'>Browse</h1>
-          <div>
-            {
-              
-            }
+          <div className='w-full'>
+            <h1 className='text-black text-xl font-bold'>Featured shows</h1>
+            <div>
+              {
+
+              }
+            </div>
           </div>
-        </div>
-      </section>
+          <div className='w-full'>
+            <h1 className='text-black text-xl font-bold'>Browse</h1>
+            <div>
+              {
+
+              }
+            </div>
+          </div>
+        </section>
+      </div>
     </div>
   );
 };
